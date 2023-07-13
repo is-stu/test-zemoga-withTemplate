e74fcbc49426629e5e34f8d1f15ff0bdc9ca26ab
Stewar Marin <stewar.marin@sofka.com.co>
2023-07-13 18:22:16 -0500

    [RF]Refactor style of survey component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -58,53 +58,58 @@ export const Card = ({ name, description, category, picture, lastUpdated, votes,
 
   return (
     <>
-      <div className='card'>
-        <div className='imageContainer'>
-          <img src={picture} alt={`Image of ${name}`} style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover'
-          }} />
-        </div>
-        <div className='Card card-info'>
-          <h2 className="featured-card__title" style={{ padding: '10px 0 0 10px' }}>{name} </h2>
-          <p className="featured-card__desc" style={{ padding: '0 10px' }}>{description}</p>
+      <div>
+        <div className='card'>
+          <div className='imageContainer'>
+            <img src={picture} alt={`Image of ${name}`} style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover'
+            }} />
+          </div>
+          <div className='Card card-info'>
+            <h2 className="featured-card__title" style={{ padding: '10px 0 0 10px' }}>{name} </h2>
+            <p className="featured-card__desc" style={{ padding: '0 10px' }}>{description}</p>
 
-        </div>
-        <div>
-          <span style={{ display: 'flex', justifyContent: 'flex-end', padding: '5px 15px' }}>
-            {
-              moment(lastUpdated).fromNow()
-            }
+          </div>
+          <div>
+            <span style={{ display: 'flex', justifyContent: 'flex-end', padding: '5px 15px' }}>
+              {
+                moment(lastUpdated).fromNow()
+              }
             &nbsp;in&nbsp;
-            {category}
-          </span>
-          {
-            !alreadyVote.find((person: string) => person === name)
-              ? (<><h3 className='info-text'>VOTE NOW !</h3>
-                <div className='card-actions' >
-                  <button className='action-button action__thumbUp' onClick={thumbsUpAction}><img src={ThumbsUp} alt="thumbs up" /></button>
-                  <button className='action-button action__thumbDown' onClick={thumbsDownAction}><img src={ThumbsDown} alt="thumbs down" /></button>
-                </div></>)
-              : (
-                <>
-                  <h3 className='info-text'>Want to vote again ?</h3>
-                  <button className='action__voteAgain' onClick={voteAgainAction}>Click here !</button>
-                </>
-              )
-          }
+              {category}
+            </span>
+            {
+              !alreadyVote.find((person: string) => person === name)
+                ? (<><h3 className='info-text'>VOTE NOW !</h3>
+                  <div className='card-actions' >
+                    <button className='action-button action__thumbUp' onClick={thumbsUpAction}><img src={ThumbsUp} alt="thumbs up" /></button>
+                    <button className='action-button action__thumbDown' onClick={thumbsDownAction}><img src={ThumbsDown} alt="thumbs down" /></button>
+                  </div></>)
+                : (
+                  <>
+                    <h3 className='info-text'>Want to vote again ?</h3>
+                    <button className='action__voteAgain' onClick={voteAgainAction}>Click here !</button>
+                  </>
+                )
+            }
+          </div>
         </div>
-
-      </div>
-      <div className='card-survey' style={{ background: `linear-gradient(to right, rgb(60, 187, 180) ${positivePercentage}%, rgb(249, 173, 29) ${negativePercentage}%)` }}>
-        <span className='card-survey__percentage'>
-          <img src={ThumbsUp} alt="Thumbs up" />
-          <span>{positivePercentage.toFixed(1)}%</span>
-        </span>
-        <span className='card-survey__percentage'>
-          <img src={ThumbsDown} alt="Thumbs down" />
-          <span>{negativePercentage.toFixed(1)}%</span>
-        </span>
+        <div style={{ display: 'flex' }}>
+          <div className='card-survey' style={{ background: 'rgba(60, 187, 180,0.8)', width: `${positivePercentage}%` }}>
+            <span className='card-survey__percentage'>
+              <img src={ThumbsUp} alt="Thumbs up" />
+              <span>{positivePercentage.toFixed(1)}%</span>
+            </span>
+          </div>
+          <div className='card-survey' style={{ background: 'rgba(249, 173, 29,0.8)', width: `${negativePercentage}%` }}>
+            <span className='card-survey__percentage'>
+              <img src={ThumbsDown} alt="Thumbs down" />
+              <span>{negativePercentage.toFixed(1)}%</span>
+            </span>
+          </div>
+        </div>
       </div>
     </>
   )
